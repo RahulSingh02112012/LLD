33c11dbfeabba02aec4af951a1527ecd55ad9fc7
Rahul Singh <rahulsingh02112012@gmail.com>
2023-12-18 16:07:39 +0530

    Added Momento/Snapshot design pattern

---
+++ src/Memento_SnapshotDesignPattern/ConfigurationCareTaker.java
@@ -0,0 +1,25 @@
+package Memento_SnapshotDesignPattern;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ConfigurationCareTaker {
+    List<CofigurationMemento> history;
+
+    public ConfigurationCareTaker() {
+        this.history = new ArrayList<>();
+    }
+
+    public void addMemento(CofigurationMemento memento) {
+        history.add(memento);
+    }
+
+    public CofigurationMemento undo() {
+        if(!history.isEmpty()) {
+            CofigurationMemento last = history.get(history.size()-1);
+            history.remove(history.size()-1);
+            return last;
+        }
+        return null;
+    }
+}
