38579df97c3298378930b44f27d9a6a574242712
Rahul Singh <rahulsingh02112012@gmail.com>
2023-12-30 23:37:29 +0530

    Added MultiThreading

---
+++ src/Multithreading/ProducerConsumerProblem/Main.java
@@ -0,0 +1,30 @@
+package Multithreading.ProducerConsumerProblem;
+
+public class Main {
+    public static void main(String[] args) {
+        SharedResource sharedResource = new SharedResource(3);
+
+        Thread producerThread = new Thread(()-> {
+            for (int i =0;i<=6;i++) {
+                try {
+                    sharedResource.produce(i);
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        Thread consumerThread = new Thread(()-> {
+            for (int i =0;i<=6;i++) {
+                try {
+                    System.out.println(sharedResource.consume());
+                } catch (InterruptedException e) {
+                    throw new RuntimeException(e);
+                }
+            }
+        });
+
+        producerThread.start();
+        consumerThread.start();
+    }
+}
---
+++ src/Multithreading/ProducerConsumerProblem/SharedResource.java
@@ -0,0 +1,40 @@
+package Multithreading.ProducerConsumerProblem;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class SharedResource {
+
+    private Queue<Integer> queue;
+    private int size;
+
+    public SharedResource(int size) {
+        queue = new LinkedList<>();
+        this.size = size;
+    }
+
+    public synchronized void produce(int item) throws InterruptedException {
+        while(queue.size() == size) {
+            System.out.println("Queue is full, Producer is waiting");
+            wait();
+        }
+
+        queue.add(item);
+        System.out.println("Item produces: " + item);
+        notify();
+    }
+
+    public synchronized int consume() throws InterruptedException {
+        while (queue.isEmpty()) {
+            System.out.println("Consumer waiting for item to be produced");
+            wait();
+        }
+
+        int item = queue.poll();
+        System.out.println("Item consumed");
+        notify();
+        return item;
+    }
+
+
+}
